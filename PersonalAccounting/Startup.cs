03324d37ebe4dcdc61e291b5b78fffc634f6e1e2
EeedMedved <eeed.medved@outlook.com>
2016-08-27 13:20:49 +0300

    Add project files.

---
+++ PersonalAccounting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PersonalAccounting.Startup))]
+namespace PersonalAccounting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
